2c1e6678813aef9556b43c8f366501cef76cd8c1
Sophia Wu <114020121@link.cuhk.edu.cn>
2023-11-01 21:21:14 -0700

    adding reduce to module

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,7 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
-import "./moduleList.css";
+import "./moduleList.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faEllipsisV, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { useSelector, useDispatch } from "react-redux";
 import {
   addModule,
@@ -9,52 +11,42 @@ import {
   updateModule,
   setModule,
 } from "./modulesReducer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faEllipsisV, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
 
 function ModuleList() {
   const { courseId } = useParams();
-  const [modules, setModules] = useParams(db.modules);
-  const [module, setModule] = useState({
-    name: "New Module",
-    description: "New Description",
-    course: courseId,
-  });
-
-  // const modules = useSelector((state) => state.modulesReducer.modules);
-  // const module = useSelector((state) => state.modulesReducer.module);
-  // const dispatch = useDispatch();
-
+  // const modules = db.modules;
+  const modules = useSelector((state) => state.modulesReducer.modules);
+  const module = useSelector((state) => state.modulesReducer.module);
+  const dispatch = useDispatch();
 
   return (
 
     <ul className="list-group">
-        <li className="list-group-item">
-          <div className="row">
-          <input value={module.name}
-          onChange={(e) => setModule({
-            ...module, name: e.target.value })}
-        />
-        <textarea value={module.description}
-          onChange={(e) => setModule({
-            ...module, description: e.target.value })}
-        />
-        {/* <input value={module.name}
-          onChange={(e) => dispatch(setModule({
-            ...module, name: e.target.value }))}
-        />
-        <textarea value={module.description}
-          onChange={(e) => dispatch(setModule({
-            ...module, description: e.target.value }))}
-        /> */}
-        </div>
-        {/* <button className="btn btn-warning" onClick={dispatch(updateModule(module))}>Update</button>
-
-        <button className="btn btn-success" onClick={() => {dispatch(addModule({...module, course: courseId}))}}>Add</button> */}
+      <li className="list-group-item">
+        <div style={{display: "flex", flexDirection: "column", }}>
+          <input 
+              value={module.name}
+              onChange={(e) =>
+                dispatch(setModule({ ...module, name: e.target.value }))
+              }/>
+          <textarea
+            value={module.description}
+            onChange={(e) =>
+              dispatch(setModule({ ...module, description: e.target.value }))
+            }/>
+          </div>
+        <button className="btn btn-success btn-sm"
+          onClick={() => dispatch(addModule({ ...module, course: courseId }))}>
+          Add
+        </button>
+        <button className="btn btn-primary btn-sm"
+          onClick={() => dispatch(updateModule(module))}>
+          Update
+        </button>
 
       </li>
-      <h5>module</h5>
+
 
       {
        modules
@@ -64,14 +56,19 @@ function ModuleList() {
               <div className="list-group-item-moduleName">
                 <h3>{module.name}</h3>
                 <div>
-                {/* <button className="btn btn-success btn-sm" onClick={() => dispatch(setModule(module)) }>Edit</button>
-                <button className="btn btn-danger btn-sm" onClick={() => dispatch(deleteModule(module._id)) }> Delete </button> */}
-
-                  {/* <button className="btn btn-success btn-sm" onClick={() => dispatch(setModule(module)) }>Edit</button>
-                <button className="btn btn-danger btn-sm" onClick={() => dispatch(deleteModule(module._id)) }> Delete </button> */}
+                  {/* <FontAwesomeIcon icon={faCircleCheck}  style={{ color: "green", paddingRight: "10px" }}/>
+                  <FontAwesomeIcon icon={faPlus} style={{ paddingRight: "10px" }}/>
+                  <FontAwesomeIcon icon={faEllipsisV} /> */}
+                  <button className="btn btn-success btn-sm"
+              onClick={() => dispatch(setModule(module))}>
+              Edit
+            </button>
+            <button className="btn btn-danger btn-sm"
+              onClick={() => dispatch(deleteModule(module._id))}>
+              Delete
+            </button>
 
                 </div>
-
               </div>
               <p>{module.description}</p>
            </li>
