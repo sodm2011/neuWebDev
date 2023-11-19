0c6ed4c52115caed66cbc2b494b728e1dc1dbd04
Sophia Wu <114020121@link.cuhk.edu.cn>
2023-11-18 17:23:36 -0800

    finish lab exercise with extra credit part

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -1,6 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 function WorkingWithArrays() {
+  const [errorMessage, setErrorMessage] = useState(null);
+
     const [todo, setTodo] = useState({
       id: 1,
       title: "NodeJS Assignment",
@@ -9,15 +12,149 @@ function WorkingWithArrays() {
       completed: false,
     });
 
+    const [todos, setTodos] = useState([]);
+    const postTodo = async () => {
+      const response = await axios.post(API, todo);
+      setTodos([...todos, response.data]);
+    };
+
+    const deleteTodo = async (todo) => {
+      try {
+        const response = await axios.delete(
+          `${API}/${todo.id}`);
+        setTodos(todos.filter((t) => t.id !== todo.id));
+      } catch (error) {
+        console.log(error)
+        setErrorMessage(error.response.data.message);
+      }
+  
+    };
+  
+    const updateTodo = async () => {
+      try {
+        const response = await axios.put(
+          `${API}/${todo.id}`, todo);
+        setTodos(todos.map((t) => (
+          t.id === todo.id ? todo : t)));
+        setTodo({});
+      } catch (error) {
+        console.log(error);
+        setErrorMessage(error.response.data.message);
+      }
+  
+    };
+  
+
+    const fetchTodos = async () => {
+      const response = await axios.get(API);
+      setTodos(response.data);
+    };
+    const removeTodo = async (todo) => {
+      const response = await axios
+        .get(`${API}/${todo.id}/delete`);
+      setTodos(response.data);
+    };
+    // const createTodo = async () => {
+    //   const response = await axios.get(`${API}/create`);
+    //   setTodos(response.data);
+    // };
+    const fetchTodoById = async (id) => {
+      const response = await axios.get(`${API}/${id}`);
+      setTodo(response.data);
+    };
+    // const updateTitle = async () => {
+    //   const response = await axios.get(
+    //     `${API}/${todo.id}/title/${todo.title}`);
+    //   setTodos(response.data);
+    // };
+  
+    useEffect(() => {
+      fetchTodos();
+    }, []);
+  
+
     const API = "http://localhost:4000/a5/todos";
     return (
       <div>
         <br/>
         <h3>Working with Arrays</h3>
+        
+
+        <input value={todo.id} readOnly />
+
+        <input className="form-control" value={todo.title}
+            onChange={(e) => setTodo({ ...todo,
+              title: e.target.value })}/>
+       <textarea
+        onChange={(e) => setTodo({ ...todo,
+          description: e.target.value })}
+        value={todo.description} type="text"
+      /> <br/>
+      <input
+        onChange={(e) => setTodo({
+          ...todo, due: e.target.value })}
+        value={todo.due} type="date"
+      /><br/>
+      <label>
+        <input
+          onChange={(e) => setTodo({
+            ...todo, completed: e.target.checked })}
+          value={todo.completed} type="checkbox"
+        />
+        Completed
+      </label>
+      <button onClick={postTodo} className="btn btn-warning mb-2 w-100">
+        Post Todo
+      </button>
+
+
+      <button onClick={updateTodo}
+              className="btn btn-success mb-2 w-100">
+        Update Todo
+      </button>
+
+      {/* <button onClick={updateTitle}
+              className="btn btn-success mb-2 w-100">
+        Update Title
+      </button> */}
+        {/* <button onClick={createTodo}
+              className="btn btn-primary mb-2 w-100">
+        Create Todo
+      </button> */}
+        {errorMessage && (
+        <div className="alert alert-danger mb-2 mt-2">
+          {errorMessage}
+        </div>
+      )}
+
+        <ul className="list-group">
+        {todos.map((todo) => (
+          <li key={todo.id}
+              className="list-group-item">
+         <button
+          onClick={() => fetchTodoById(todo.id)}
+          className="btn btn-warning me-2 float-end" >
+          Edit
+        </button>
+          <button
+          onClick={() => deleteTodo(todo)}
+          className="btn btn-danger float-end" >
+          Delete
+        </button>
+        <input
+              checked={todo.completed}
+              type="checkbox" readOnly
+            />
+            {todo.title}
+            <p>{todo.description}</p>
+            <p>{todo.due}</p>          </li>
+        ))}
+      </ul>
         <h4>1. Retrieving Arrays</h4>
             <a href={API} className="btn btn-primary me-2">
             Get Todos
             </a>
+          
 
         <h4>2. Retrieving an Item from an Array by ID</h4>
             <input
--- src/Labs/a5/WorkingWithObjects.js
+++ src/Labs/a5/WorkingWithObjects.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function WorkingWithObjects() {
     const [assignment, setAssignment] = useState({
         id: 1,
@@ -9,23 +11,62 @@ function WorkingWithObjects() {
         score: 0,
       });
       const URL = "http://localhost:4000/a5/assignment";
+      const fetchAssignment = async () => {
+        const response = await axios.get(URL);
+        setAssignment(response.data);
+      };
+
+      const updateTitle = async () => {
+        const response = await axios
+          .get(`${URL}/title/${assignment.title}`);
+        setAssignment(response.data);
+      };
+      useEffect(() => {
+        fetchAssignment();
+      }, []);
 
   return (
     <div>
       <h3>Working With Objects</h3>
       <h4>Modifying Properties</h4>
-      <a
-        href={`${URL}/title/${assignment.title}`}
-        className="btn btn-primary me-2 float-end"
-      >
-        Update Title
-      </a>
       <input
         onChange={(e) => setAssignment({ ...assignment,
             title: e.target.value })}
         value={assignment.title}
         className="form-control mb-2 w-75"
         type="text" />
+      <button onClick={updateTitle}
+              className="w-100 btn btn-primary mb-2">
+        Update Title to: {assignment.title}
+      </button>
+
+      <div>
+      <h4>Assignment id</h4>
+      <span>{assignment.id}</span>
+      <h4>Assignment Title</h4>
+      {assignment.title}
+      <h4>Assignment Description</h4>
+      {assignment.description}
+      <h4>Assignment Due</h4>
+      {assignment.due}
+      <h4>Assignment Completed</h4>
+      {assignment.completed ? "Yes" : "No"}
+      <h4>Assignment Score</h4>
+      {assignment.score}
+      </div>
+      <button onClick={fetchAssignment}
+              className="w-100 btn btn-danger mb-2">
+        Fetch Assignment
+      </button>
+
+
+      <a
+        href={`${URL}/title/${assignment.title}`}
+        className="btn btn-primary me-2 float-end"
+      >
+        Update Title
+      </a>
+
 
 <a
         href={`${URL}/score/${assignment.score}`}
